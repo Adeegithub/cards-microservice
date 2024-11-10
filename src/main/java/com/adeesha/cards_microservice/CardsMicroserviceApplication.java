cbf8a66e1f82f3d375d723963f43d4210796b4f4
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-11-10 18:10:55 +0530

    Inserted @EnableJpaAuditing to enable Spring auditing and Enhanced the Documentation.

--- src/main/java/com/adeesha/cards_microservice/CardsMicroserviceApplication.java
+++ src/main/java/com/adeesha/cards_microservice/CardsMicroserviceApplication.java
@@ -1,9 +1,36 @@
 package com.adeesha.cards_microservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Adeesha Gunawardana",
+						email = "adeesha.ag@gmail.com",
+						url = "https://adeegithub.github.io/my-portfolio/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/Adeegithub"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Cards microservice REST API Documentation",
+				url = "http://localhost:9000/swagger-ui/index.html#/accounts-controller/"
+		)
+)
 public class CardsMicroserviceApplication {
 
 	public static void main(String[] args) {
