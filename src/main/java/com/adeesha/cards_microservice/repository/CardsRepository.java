47c70d73dbbddb740a79fab017f113423cbc91bc
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-11-10 16:28:36 +0530

    Implemented Cards Repository

---
+++ src/main/java/com/adeesha/cards_microservice/repository/CardsRepository.java
@@ -0,0 +1,13 @@
+package com.adeesha.cards_microservice.repository;
+
+import com.adeesha.cards_microservice.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+    Optional<Cards> findByCardNumber(String cardNumber);
+}
