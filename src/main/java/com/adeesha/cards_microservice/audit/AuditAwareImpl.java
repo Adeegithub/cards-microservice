fcd13200118074aa6b7f05f64e738a1dd1e327e1
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-11-10 18:14:59 +0530

    Changed the username of H2 Database and Inserted @Component("auditAwareImpl") in AuditAwareImpl class.

--- src/main/java/com/adeesha/cards_microservice/audit/AuditAwareImpl.java
+++ src/main/java/com/adeesha/cards_microservice/audit/AuditAwareImpl.java
@@ -2,9 +2,11 @@ package com.adeesha.cards_microservice.audit;
 
 import lombok.NonNull;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component("auditAwareImpl")
 public class AuditAwareImpl implements AuditorAware<String> {
     @Override
     @NonNull
